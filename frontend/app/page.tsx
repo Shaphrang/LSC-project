b5d551754d62760389f41bd4ae8e7a3170f23811
Shaphrang <Shabzryngksai@gmail.com>
2026-01-21 22:51:07 +0530

    Initial push

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">
+        MSRLS–LSC MIS
+      </h1>
+      <p className="mt-2 text-gray-600">
+        Frontend setup successful.
+      </p>
+    </main>
+  );
+}
